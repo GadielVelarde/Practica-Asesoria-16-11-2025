9e5779656a9123321d71f77e91eb884d6f48e30b
GadielVelarde <jesus.velarde@utec.edu.pe>
2025-11-24 01:48:41 -0500

    arregla

--- backend/src/main/java/com/backend/backend/Carrito/application/CarritoControllerImpl.java
+++ backend/src/main/java/com/backend/backend/Carrito/application/CarritoControllerImpl.java
@@ -1,21 +1,30 @@
 package com.backend.backend.Carrito.application;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.backend.backend.Carrito.domain.CarritoServiceImpl;
 import com.backend.backend.Carrito.dto.AddItemCarritoDTO;
 import com.backend.backend.Carrito.dto.CarritoDTO;
 import com.backend.backend.Carrito.dto.UpdateItemCarritoDTO;
 import com.backend.backend.common.dto.ResponseDTO;
 import com.backend.backend.users.domain.Usuario;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/carrito")
-@RequiredArgsConstructor "abcdg"
+@RequiredArgsConstructor
 public class CarritoControllerImpl {
 
     private final CarritoServiceImpl carritoService;
