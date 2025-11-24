06360d6e287bec9bc60049639f8df60238f1c26a
GadielVelarde <jesus.velarde@utec.edu.pe>
2025-11-24 01:30:40 -0500

    maq1

--- backend/src/main/java/com/backend/backend/users/domain/TipoUsuario.java
+++ backend/src/main/java/com/backend/backend/users/domain/TipoUsuario.java
@@ -1,6 +1,22 @@
 package com.backend.backend.users.domain;
 
-public enum TipoUsuario {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public enum TipoUsuario implements GrantedAuthority {
     CLIENTE,
-    COMERCIANTE
+    COMERCIANTE;
+
+    public GrantedAuthority asAuthority() {
+        return new SimpleGrantedAuthority(getAuthority());
+    }
+
+    public String getRoleName() {
+        return getAuthority();
+    }
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name();
+    }
 }
